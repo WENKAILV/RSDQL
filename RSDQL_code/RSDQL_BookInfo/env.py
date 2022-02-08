230f7fce9a6642d506d875d7833505a5693ddb45
Cyan99 <48341732+Cyan99@users.noreply.github.com>
2022-02-08 15:32:12 +0800

    Update env.py

--- RSDQL_code/RSDQL_BookInfo/env.py
+++ RSDQL_code/RSDQL_BookInfo/env.py
@@ -3,10 +3,10 @@ NodeNumber = 5
 ServiceNumber = 4
 ResourceType = 2
 service_containernum = [1,1,3,1]
-service_container = [[0],[1],[2,3,4],[5]] # 服务需要哪几个container完成
+service_container = [[0],[1],[2,3,4],[5]] 
 service_container_relationship = [0,1,2,2,2,3]
-alpha = 0.5 # reward权重因子
-beta =[0.5,0.5] # 不同资源的重要程度
+alpha = 0.5 # reward weighting factor
+beta =[0.5,0.5]
 count = 0
 CPUnum = 4
 Mem = 4*1024
@@ -18,7 +18,7 @@ import agent
 
 class Env():
     def __init__(self):
-        # 整体state
+        # State
         self.State = []
         self.node_state_queue = []
         self.container_state_queue = []
@@ -26,35 +26,21 @@ class Env():
         self.prepare()
 
     def prepare(self):
-        # ra = np.random.RandomState(0)
-        # for i in range(ContainerNumber):
-        #     self.container_state_queue.extend ([-1,ra.uniform(0.1,0.4),ra.uniform(0.1,0.4)])
         self.container_state_queue = [-1,0.5/CPUnum,128/Mem , -1,0.5/CPUnum,256/Mem , -1,0.5/CPUnum,256/Mem, -1,0.5/CPUnum,256/Mem, -1,0.5/CPUnum,256/Mem, -1,0.5/CPUnum,128/Mem]
-        #print("初始container state",self.container_state_queue)
 
         for i in range(NodeNumber):
             self.node_state_queue.extend( [ 0,0,0,0,0,0, 0 , 0 ] )
         self.State = self.container_state_queue + self.node_state_queue
         self.action = [-1,-1]
         self.action_queue = [-1,-1]
-    # 构建微服务权重、节点间通信距离的数组
-        # 微服务之间的通信权重，1，10之间
-        #self.service_weight = [[0,1,0,2,0,0],[1,0,4,0,4,0],[0,4,0,0,0,0],[2,0,0,0,4,1],[0,4,0,4,0,0],[0,0,0,1,0,0]]
+   
+        # Communication weight between microservices
         self.service_weight = [[0,1,0,0],[1,0,1,0],[0,1,0,2],[0,0,2,0]]
-        # np.random.seed(1)
-        # self.service_weight = np.random.randint(1,10,(ServiceNumber,ServiceNumber))
-        # self.service_weight = np.triu(self.service_weight)
-        # self.service_weight += self.service_weight.T - 2 * np.diag(self.service_weight.diagonal())
-        # 节点之间的通信距离Dist，随机生成范围在1，10的二维数组
+        # Communication distance between nodes
         self.Dist = [[0,1,1,1,1],[1,0,1,1,1],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
-        # self.Dist = np.random.randint(1,10,(NodeNumber,NodeNumber))
-        # self.Dist = np.triu(self.Dist)
-        # self.Dist += self.Dist.T - 2 * np.diag(self.Dist.diagonal())
-        #print("权重",self.service_weight)
-        #print("通信距离",self.Dist)
 
     def ContainerCost(self,i,j):
-    # 计算容器i、j间的距离
+    # to calculate the distance between container i and j
         m = -1
         n = -1
         m = self.container_state_queue[i*3]
@@ -70,18 +56,15 @@ class Env():
         return container_dist
 
     def CalcuCost(self,i,j):
-    # 计算服务i、j间的通信开销
-        # 服务i、j间的通信权重
+    # to calculate the communication cost between container i and j
         cost = 0
         interaction = self.service_weight[i][j] / (service_containernum[i] * service_containernum[j])
         for k in range ( len(service_container[i]) ):
             for l in range (len(service_container[j]) ):
                 cost += self.ContainerCost(service_container[i][k],service_container[j][l]) * interaction
-        #print("服务",i,"和服务",j,"的通信开销为",cost)
         return cost
 
     def sumCost(self):
-    # 总通信开销
         Cost = 0
         for i in range (ServiceNumber):
             for j in range (ServiceNumber):
@@ -99,9 +82,8 @@ class Env():
             NodeMemory.append(M)
             # if NodeCPU[i] > 1 or NodeMemory[i] > 1:
             #     Var -= 10  
-        # 节点负载的方差
+        # Variance of node load
         Var += beta[0] * np.var(NodeCPU) + beta[1] * np.var(NodeMemory)
-        #print("节点负载的方差为：",Var)
         return Var
 
     def cost(self):
@@ -110,11 +92,6 @@ class Env():
         g1 = g1 / 4
         g2 = self.CalcuVar()
         g2 = g2 / 0.052812500000000005
-
-        # if g2 > 1:
-        #     re = -10
-        # print("归一化后的通信开销",g1)
-        # print("归一化后的节点负载方差",g2)
         re += alpha *  g1  + (1-alpha) *  g2 
         return 100*re,g1,g2
 
@@ -122,13 +99,11 @@ class Env():
         self.State = container_state_queue + node_state_queue
 
     def update(self):
-    # state更新
-        # print("action node",self.action[0])
-        # print("action container",self.action[1])
+    # update state
         if self.action[0] >= 0 and self.action[1] >= 0:
-            # 更新容器状态
+            # update container state
             self.container_state_queue[ self.action[1] * 3 ] = self.action[0] 
-            # 更新节点状态
+            # update node state
             self.node_state_queue[ self.action[0] * (ContainerNumber+2) + self.action[1] ] = 1
             self.node_state_queue[ self.action[0] * (ContainerNumber+2) + ContainerNumber] += self.container_state_queue[ self.action[1] * 3 + 1 ]
             self.node_state_queue[ self.action[0] * (ContainerNumber+2) + (ContainerNumber + 1) ] += self.container_state_queue[ self.action[1] * 3 + 2 ]
@@ -141,17 +116,14 @@ class Env():
 
             self.prepare()  
         self.state_update(self.container_state_queue,self.node_state_queue)
-        #print("更新后的State",self.State)
         return self.State
 
     def step(self,action):
-    # 输入action（Targetnode，ContainerIndex），输出下一状态、cost、done
+    # input: action(Targetnode，ContainerIndex)
+    # output: next state, cost, done
         global count
-        # 把一维action转成二维
         self.action = self.index_to_act(action)
-        # 更新state
         self.update()
-        # 计算reward
         cost,comm,var = self.cost()   
         done = False 
         count = 0
@@ -174,28 +146,3 @@ class Env():
         act[0] = int(index / ContainerNumber)
         act[1] = index % ContainerNumber
         return act
-
-# env2 = Env()
-# env2.CalcuCost(0,1)
-# #a,b,c = env2.step(17)
-# # print(a)
-# # print(b)
-# # print(c)
-#env2.step(13)
-# env2.step(6)
-# env2.CalcuCost(0,1)
-
-#env2.update()
-#env2.CalcuVar()
-#env2.prepare()
-# # env2.update()
-# #print(len(env2.State))
-# # # #env2.reward()
-#env2.update((3,4))
-
-# env2.step([1,0])
-#env2.CalcuNodeI(1)
-#env2.reward()
-# print(env2.container_state_queue[0][0])
-# print (env2.State(env2.container_state_queue,env2.node_state_queue),env2.action)
-

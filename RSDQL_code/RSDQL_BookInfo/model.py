d4c5f8c0839694a1193cf11109ba34dbe3a3b1e9
Cyan99 <48341732+Cyan99@users.noreply.github.com>
2022-02-08 15:32:34 +0800

    Update model.py

--- RSDQL_code/RSDQL_BookInfo/model.py
+++ RSDQL_code/RSDQL_BookInfo/model.py
@@ -15,14 +15,12 @@
 #-*- coding: utf-8 -*-
 
 import parl
-from parl import layers  # 封装了 paddle.fluid.layers 的API
-
-
+from parl import layers  
 class Model(parl.Model):
     def __init__(self, act_dim):
         hid1_size = 128
         hid2_size = 128
-        # 3层全连接网络
+
         self.fc1 = layers.fc(size=hid1_size, act='relu')
         self.fc2 = layers.fc(size=hid2_size, act='relu')
         self.fc3 = layers.fc(size=act_dim, act=None)
